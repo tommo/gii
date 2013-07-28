0f426c2e44d4676f3ec8b2d73262a02e43098b58
tommo <tommo.zhou@gmail.com>
2013-07-28 23:36:49 +0800

    add ide command

---
+++ tools/ide/__init__.py
@@ -0,0 +1,12 @@
+import os.path
+import logging
+
+from gii.core import Project, app
+
+def main( argv ):
+	app.openProject()
+	
+	# import gii.moai.MOAIGameView
+	import gii.AssetBrowser
+
+	app.run()
