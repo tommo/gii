9d02fd19cabb6ec1d23b3632d6a04b3f7d2412da
tommo <tommo.zhou@gmail.com>
2014-02-07 16:23:08 +0800

    prevent spinbox get wheel focus

--- lib/gii/qt/controls/PropertyEditor/FieldEditorControls.py
+++ lib/gii/qt/controls/PropertyEditor/FieldEditorControls.py
@@ -7,8 +7,8 @@ class FieldEditorLineEdit(QtGui.QLineEdit):
 		super(FieldEditorLineEdit, self).__init__( *arg )
 
 	def focusInEvent( self, ev ):
-		self.selectAll()
 		super(FieldEditorLineEdit, self).focusInEvent( ev )
+		self.selectAll()
 		
 
 ##----------------------------------------------------------------##
@@ -16,22 +16,44 @@ class FieldEditorSpinBox(QtGui.QSpinBox):
 	def __init__(self, *arg):
 		super(FieldEditorSpinBox, self).__init__( *arg )
 		self.setButtonSymbols( QtGui.QAbstractSpinBox.NoButtons )
-
+		self.setFocusPolicy( Qt.StrongFocus )
 
 	def focusInEvent( self, ev ):
-		self.selectAll()
 		super(FieldEditorSpinBox, self).focusInEvent( ev )
-		
+		self.selectAll()
+		self.setFocusPolicy( Qt.WheelFocus )
+
+	def focusOutEvent( self, ev ):
+		super(FieldEditorSpinBox, self).focusOutEvent( ev )
+		self.setFocusPolicy( Qt.StrongFocus )
+
+	def wheelEvent( self, ev ):
+		if self.hasFocus():
+			super(FieldEditorSpinBox, self).wheelEvent( ev )
+		else:
+			ev.ignore()
+
 ##----------------------------------------------------------------##
 class FieldEditorDoubleSpinBox(QtGui.QDoubleSpinBox):
 	def __init__(self, *arg):
 		super(FieldEditorDoubleSpinBox, self).__init__( *arg )
 		self.setButtonSymbols( QtGui.QAbstractSpinBox.NoButtons )
+		self.setFocusPolicy( Qt.StrongFocus )
 
 	def focusInEvent( self, ev ):
-		self.selectAll()
 		super(FieldEditorDoubleSpinBox, self).focusInEvent( ev )
+		self.selectAll()
+		self.setFocusPolicy( Qt.WheelFocus )
+
+	def focusOutEvent( self, ev ):
+		super(FieldEditorDoubleSpinBox, self).focusOutEvent( ev )
+		self.setFocusPolicy( Qt.StrongFocus )
 		
+	def wheelEvent( self, ev ):
+		if self.hasFocus():
+			super(FieldEditorDoubleSpinBox, self).wheelEvent( ev )
+		else:
+			ev.ignore()
 
 ##----------------------------------------------------------------##
 class DraggableLabel( QtGui.QLabel ):
